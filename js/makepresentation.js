26489227f8388cfbe97fcefc60016d29408db9dc
Chris Heilmann <chris.heilmann@gmail.com>
2011-09-15 09:08:35 +0100

    embeddable

--- js/makepresentation.js
+++ js/makepresentation.js
@@ -1,20 +1,29 @@
 (function(){
   var slides = document.querySelectorAll( 'body > section' );
-  for(var i = 1; slides[i]; i++) {
-    slides[i].innerHTML += '<input type="checkbox" checked class="check">';
-    slides[i].className += ' active';
-  }
-  slides[0].innerHTML += '<p class="presentationinfo">Uncheck the slides below that you don\'t want to include and press the button below to start the presentation. In presentation mode press the N key to toggle the display of notes. <button>Start Presentation</button></p>'; 
-  
-  var button = slides[ 0 ].querySelector( 'button' );
+  var newidx = ~~window.location.hash.split( "#" )[ 1 ];
+  if ( newidx ) {
+    setup();
+  } else {
+    for(var i = 1; slides[i]; i++) {
+      slides[i].innerHTML += '<input type="checkbox" checked class="check">';
+      slides[i].className += ' active';
+    }
+    slides[0].innerHTML += '<p class="presentationinfo">Uncheck the slides below that you don\'t want to include and press the button below to start the presentation. In presentation mode press the N key to toggle the display of notes. <button>Start Presentation</button></p>'; 
 
-  button.addEventListener( 'click', function( ev ) {
-    for( var i = 1; slides[i]; i++ ) {
-      if( slides[ i ].className.indexOf( 'remove' ) !== -1 ) {
+    var button = slides[ 0 ].querySelector( 'button' );
+
+    button.addEventListener( 'click', setup ,false );
+    
+  }
+
+  function setup( ev ) {
+    for ( var i = 1; slides[i]; i++ ) {
+      if ( slides[ i ].className.indexOf( 'remove' ) !== -1 ) {
         slides[ i ].parentNode.removeChild( slides[ i ] );
       }
     }
-    document.querySelector( '.presentationinfo' ).style.display = 'none';
+    var info = document.querySelector( '.presentationinfo' );
+    if ( info ) { info.style.display = 'none'; }
     document.querySelector( 'link' ).disabled = true;
     var link = document.createElement( 'link' );
     link.href = 'styles/styles.css';
@@ -23,8 +32,8 @@
     var s = document.createElement( 'script' );
     s.src = 'js/dzslides.js';
     document.querySelector( 'head' ).appendChild( s );
-    ev.preventDefault();
-  },false);
+    if(ev){ ev.preventDefault(); }
+  }
 
   document.addEventListener( 'click', function( ev ) {
     var t = ev.target;
